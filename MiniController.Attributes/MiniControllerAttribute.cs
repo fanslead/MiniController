3d46aa8ed67150038c034ce0dc0ca994c5cb7bf9
刘俊繁_JF_L <liujunfan@vip.qq.com>
2025-07-16 14:02:39 +0800

    chore: remove useless code

--- MiniController.Attributes/MiniControllerAttribute.cs
+++ MiniController.Attributes/MiniControllerAttribute.cs
@@ -11,11 +11,6 @@ public class MiniControllerAttribute : Attribute
     public string? RoutePrefix { get; }
     
     /// <summary>
-    /// 端点组名称，用于API文档分组
-    /// </summary>
-    public string? Name { get; set; }
-    
-    /// <summary>
     /// 端点过滤器类型
     /// </summary>
     public Type? FilterType { get; set; }
